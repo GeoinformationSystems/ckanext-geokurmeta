124f7687ba8a6d52d83a573c5245111062c561e1
rue_a <arne.ruemmler@gmail.com>
2021-01-20 17:31:48 +0100

    validation

--- ckanext/geokurmeta/validation.py
+++ ckanext/geokurmeta/validation.py
@@ -8,9 +8,10 @@ from six import string_types, iteritems
 from six.moves.urllib.parse import urlparse
 from decimal import Decimal, DecimalException
 
+
 def if_not_missing_package_id_or_name_exists(value, context):
     if value:
-    
+
         model = context['model']
         session = context['session']
 
@@ -24,18 +25,20 @@ def if_not_missing_package_id_or_name_exists(value, context):
 
     return value
 
+
 def if_not_missing_package_id_or_name_exists_list(key, data, errors, context):
     '''Takes a list of package identifiers that is a comma-separated string and validates each.'''
     if isinstance(data[key], string_types):
-        datasets = [ds.strip() \
-                for ds in data[key].split(',') \
-                if ds.strip()]
+        datasets = [ds.strip()
+                    for ds in data[key].split(',')
+                    if ds.strip()]
     else:
         datasets = data[key]
 
     for ds in datasets:
         if_not_missing_package_id_or_name_exists(ds, context)
-    
+
+
 def single_link_validator(value, context):
     ''' Checks that the provided value (if it is present) is a valid URL '''
 
@@ -44,24 +47,26 @@ def single_link_validator(value, context):
         if all([pieces.scheme, pieces.netloc]) and \
            set(pieces.netloc) <= set(string.ascii_letters + string.digits + '-.') and \
            pieces.scheme in ['http', 'https']:
-           return value
+            return value
         else:
-            raise Invalid('Please provide a valid link: %s' % value)    
+            raise Invalid('Please provide a valid link: %s' % value)
     return
-    
+
+
 def link_list_string_convert(key, data, errors, context):
     '''Takes a list of links that is a comma-separated string and validates each.'''
 
     if isinstance(data[key], string_types):
-        links = [link.strip() \
-                for link in data[key].split(',') \
-                if link.strip()]
+        links = [link.strip()
+                 for link in data[key].split(',')
+                 if link.strip()]
     else:
         links = data[key]
 
     for link in links:
         single_link_validator(link, context)
-     
+
+
 def decimal_validator(value, context):
     ''' Checks that the provided value (if it is present) is a valid decimal '''
 
@@ -69,7 +74,5 @@ def decimal_validator(value, context):
         try:
             Decimal(value)
         except DecimalException:
-            raise Invalid('Invalid decimal')    
+            raise Invalid('Invalid decimal')
     return value
-
-    
