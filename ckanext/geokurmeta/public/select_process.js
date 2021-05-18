66aa920c59cdd3ec9f4b20ef0d3fd869ec5f52b0
rue_a <arne.ruemmler@gmail.com>
2021-05-18 22:36:57 +0200

    ''

--- ckanext/geokurmeta/public/select_process.js
+++ ckanext/geokurmeta/public/select_process.js
@@ -64,7 +64,7 @@ this.ckan.module('select-process', function (jQuery, _) {
             var btn = $("#" + this.options.field + "-btn")[0];
 
             $(this.el[0]).on("change", function () {
-                console.log($(this.el[0]).children("option:selected").val())
+                console.log($(this).children("option:selected").val())
             })
 
 
