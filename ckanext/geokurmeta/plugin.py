242af26865dd26ca4d63d77aae295f9da9b5d551
rue_a <arne.ruemmler@gmail.com>
2021-05-18 14:08:45 +0200

    test something

--- ckanext/geokurmeta/plugin.py
+++ ckanext/geokurmeta/plugin.py
@@ -1,6 +1,9 @@
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
+import routes.mapper
+import ckan.lib.base as base
+
 from ckanext.geokurmeta.validation import (
     geokurmeta_scheming_choices,
     geokurmeta_check_required_field,
@@ -23,6 +26,7 @@ class GeokurmetaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
     p.implements(p.IConfigurer)
     p.implements(p.IValidators)
     p.implements(p.ITemplateHelpers)
+    p.implements(p.IRoutes)
 
     def get_validators(self):
         return {
@@ -34,14 +38,14 @@ class GeokurmetaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
             'single_link_validator': single_link_validator,
             'decimal_validator': decimal_validator,
             'spatial_resolution_validator': spatial_resolution_validator,
-            }
- 
+        }
+
     def get_helpers(self):
         return {
             'get_link_list': get_link_list,
             'get_json_as_dict': get_json_as_dict,
-            }    
-    
+        }
+
     def is_fallback(self):
         # Return True to register this plugin as the default handler for
         # package types not handled by any other IDatasetForm plugin.
@@ -51,13 +55,25 @@ class GeokurmetaPlugin(p.SingletonPlugin, tk.DefaultDatasetForm):
         # This plugin doesn't handle any special package types, it just
         # registers itself as the default (above).
         return []
-        
-    def update_config(self, config):    
+
+    def update_config(self, config):
         # Add this plugin's templates dir to CKAN's extra_template_paths, so
         # that CKAN will use this plugin's custom templates.
         tk.add_template_directory(config, 'templates')
         tk.add_public_directory(config, 'public')
         tk.add_resource('public', 'ckanext-geokurmeta')
-        
-        
 
+    def before_map(self, route_map):
+        with routes.mapper.SubMapper(route_map,
+                                     controller='ckanext.geokurmeta.plugin:GeokurmetaController') as m:
+            m.connect('contentPage', '/contentPage', action='contentPage')
+        return route_map
+
+    def after_map(self, route_map):
+        return route_map
+
+
+class GeokurmetaController(base.BaseController):
+
+    def contentPage(self):
+        return base.render('content/contentPage.html')
